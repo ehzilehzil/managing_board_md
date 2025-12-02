29099d406750531f8c5044d8e4df3a6a68ac9eb1
ehzilehzil <ehzilehzil@outlook.com>
2025-12-02 21:54:12 +0900

    f

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,19 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
+import alpinejs from '@astrojs/alpinejs';
+
+
+
 // https://astro.build/config
-export default defineConfig({});
+const isDev = process.env.NODE_ENV === 'development';
+export default defineConfig({
+  base: isDev ? '/' : '/managing_board',
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [alpinejs()]
+});
